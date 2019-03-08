37728838cdc2a383bf1251286632cdc121db34d9
Andrew Michaud <dev@drew.life>
2019-03-08 01:00:33 -0800

    pin weatherbotskeleton fixing send issues

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["weatherlies_bot = weatherlies_bot.__main__:main"]
       },
 
-      install_requires=["weatherbotskeleton>=1.4.2"],
+      install_requires=["weatherbotskeleton>=1.4.3"],
       python_requires=">=3.6",
 
       license="BSD3",
