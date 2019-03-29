e1bf39ea8a1be23a5ca5696d9ec00e34ab327888
Andrew Michaud <dev@drew.life>
2019-03-29 13:29:21 -0700

    fix weatherliesbot not actually being on the weatherbotskeleton

--- weatherlies_bot/__main__.py
+++ weatherlies_bot/__main__.py
@@ -1,34 +1,33 @@
 """Main class to run weatherlies."""
 
-import os
 import random
-import time
+from os import path
 
-import botskeleton
-
-import weather_gen
+import weatherbotskeleton
 
 # Delay between tweets in seconds.
 DELAY = 3600
 DELAY_VARIATION = 369
 
 if __name__ == "__main__":
-    SECRETS_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "SECRETS")
-    BOT_SKELETON = botskeleton.BotSkeleton(
+    SECRETS_DIR = path.join(path.abspath(path.dirname(__file__)), "SECRETS")
+    BOT_SKELETON = weatherbotskeleton.WeatherbotSkeleton(
         secrets_dir=SECRETS_DIR,
         owner_url="https://github.com/alixnovosi/weatherlies_bot",
         bot_name="weatherlies_bot",
         cities_file=path.join(SECRETS_DIR, "city.list.json"),
+        lies=True
     )
 
     LOG = BOT_SKELETON.log
 
     while True:
-        weather = weather_gen.produce_status()
+        LOG.info("Sending a weather tweet.")
+        weather = BOT_SKELETON.produce_status()
 
         LOG.info(f"Sending:\n {weather}")
         BOT_SKELETON.send(weather)
 
-        BOT_SKELETON.delay = random.choice(range(DELAY-DELAY_VARIATION,
-                                                 DELAY+DELAY_VARIATION+1))
+        BOT_SKELETON.set_delay(random.choice(range(DELAY-DELAY_VARIATION,
+                                                   DELAY+DELAY_VARIATION+1))
         BOT_SKELETON.nap()
